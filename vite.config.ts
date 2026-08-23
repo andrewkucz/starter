9c5077b88123d4a7543b84423342d891d8bce27a
Andrew Kuczynski <kuczynskiandrew@gmail.com>
2026-08-23 15:57:46 -0400

    init tanstack start

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, lazyPlugins } from "vite-plus";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import neon from "./neon-vite-plugin.ts";
+
+const config = defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  resolve: { tsconfigPaths: true },
+  plugins: lazyPlugins(() => [
+    devtools(),
+    netlify(),
+    neon,
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ]),
+});
+
+export default config;

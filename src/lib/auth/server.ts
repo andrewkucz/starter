e58dac84178fd4cabec72e85cc12a69c43a299d8
andrewkucz <kuczynskiandrew@gmail.com>
2026-08-24 18:37:02 -0400

    better-auth wiring

--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -1,7 +1,12 @@
 import { betterAuth } from "better-auth";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { Pool } from "pg";
+import { getConnectionString } from "@netlify/database";
 
 export const auth = betterAuth({
+  database: new Pool({
+    connectionString: getConnectionString(),
+  }),
   emailAndPassword: {
     enabled: true,
   },
